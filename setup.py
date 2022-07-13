ac2e78a2e16b13df80ebc32ca8fe3e86c83bd289
omidekz <omidekz@gmail.com>
2022-07-14 00:50:47 +0430

    fix

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir = {"faclassbase": "faclassbase"},
+    package_dir = {"": "faclassbase"},
     packages = setuptools.find_packages(where="faclassbase"),
     python_requires = ">=3.6"
 )
