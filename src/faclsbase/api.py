3ae02fffb5205e844e9a66dcfdc9a2cfa5c95b19
omidekz <omidekz@gmail.com>
2022-07-14 13:51:01 +0430

    make func with better name to increase readability:))

--- src/faclsbase/api.py
+++ src/faclsbase/api.py
@@ -5,6 +5,9 @@ from pydantic.main import ModelMetaclass
 
 
 
+instantiate_and_call_run = lambda _class: lambda **kwargs: _class(**kwargs).run()
+
+
 class BaseAPI(BaseModel):
     class Config:
         arbitrary_types_allowed = True
@@ -15,10 +18,20 @@ class BaseAPI(BaseModel):
 
 
 class API(ModelMetaclass):
+    """
+        API is a metaclass that it's goal is
+        coverts `__init__` to a function to call `__init__()` and return `obj.run()`
+        
+    """
+
+    @staticmethod
+    def convert__init__to_instantiate_and_call_run_lambda(_class):
+        return makefun.create_function(
+            signature(_class), instantiate_and_call_run(_class), _class.__name__)
+
     def __new__(cls, _, bases, dct):
         bases = list(bases)
         if BaseAPI not in bases:
             bases = bases + [BaseAPI]
         class_ = super().__new__(cls, _, tuple(bases), dct)
-        return makefun.create_function(
-            signature(class_), lambda *args, **kwargs: class_(*args, **kwargs).run(), class_.__name__)
+        return cls.convert__init__to_instantiate_and_call_run_lambda(class_)
