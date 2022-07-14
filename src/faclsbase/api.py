2774a5ca13144fccfca3263351c3d9c096853b36
omidekz <omidekz@gmail.com>
2022-07-14 13:54:19 +0430

    remove useless lambda and ajaghvajagh

--- src/faclsbase/api.py
+++ src/faclsbase/api.py
@@ -1,33 +1,21 @@
 from inspect import signature
 import makefun
-from pydantic import BaseModel
 from pydantic.main import ModelMetaclass
-
-
-
-instantiate_and_call_run = lambda _class: lambda **kwargs: _class(**kwargs).run()
-
-
-class BaseAPI(BaseModel):
-    class Config:
-        arbitrary_types_allowed = True
-        extra = 'ignore'
-
-    def run(self):
-        raise NotImplementedError
+from .base_api import BaseAPI
 
 
 class API(ModelMetaclass):
     """
         API is a metaclass that it's goal is
         coverts `__init__` to a function to call `__init__()` and return `obj.run()`
-        
+
     """
 
     @staticmethod
-    def convert__init__to_instantiate_and_call_run_lambda(_class):
+    def convert__init__to_instantiate_and_call_run(_class):
+        instantiate_and_call_run = lambda **kwargs: _class(**kwargs).run()
         return makefun.create_function(
-            signature(_class), instantiate_and_call_run(_class), _class.__name__)
+            signature(_class), instantiate_and_call_run, _class.__name__)
 
     def __new__(cls, _, bases, dct):
         bases = list(bases)
